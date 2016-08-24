b0d9618c087850753bdd459a28ee0b27280a02aa
tspalartresilient <t.spalart@resilientplc.com>
2016-08-24 11:59:20 +0100

    Left to Right Average Monochrom

--- js/main.js
+++ js/main.js
@@ -110,7 +110,7 @@ function updateAnalysers(time) {
     {
         var SPACING = 3;
         var BAR_WIDTH = 2;
-        var numBars = Math.round(canvasWidth / SPACING);
+        var numBars = 1;
         var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
 
         analyserNode.getByteFrequencyData(freqByteData);
